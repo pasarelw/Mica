a1aee355d76ddd1733237f91799c178e38ea88b8
pasarelw <paserebogdan@icloud.com>
2026-01-31 20:21:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://music.youtube.com/watch?v=EIrawjKW5G8", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/daco4h3xg/video/upload/v1769883593/UNAE_EIrawjKW5G8_nxnbvu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
